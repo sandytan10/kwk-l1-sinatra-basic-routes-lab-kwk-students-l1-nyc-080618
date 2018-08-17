50e469455764d3d926c2afbc81fc505608446982
sandytan10 <stan10@stuy.edu>
2018-08-17 13:32:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get "/name" do 
+ "Hello my name is Sandy" 
+end   
+
+get "/hometown" do
+  "My hometown is New York"
+end 
+
+get "/favorite" do 
+  "My favorite song is Sober"
 end
+
+end
+
+
